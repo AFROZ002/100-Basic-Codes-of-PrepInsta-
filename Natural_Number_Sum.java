edac5e4bf2a13a30d0f0e9fdcc4d083a0c2f19d3
Afroz Alam <93541431+AFROZ002@users.noreply.github.com>
2023-11-02 23:04:43 +0530

    Add files via upload
     Final Commit

---
+++ Natural_Number_Sum.java
@@ -0,0 +1,12 @@
+package PrepInsta_100_Program;
+
+public class Natural_Number_Sum {
+    public static void main(String[] args) {
+        int num=10;
+        int sum=0;
+        for (int i=1;i<=num;++i){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
