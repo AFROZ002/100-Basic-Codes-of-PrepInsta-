778d245bf1275e2f4360ce516087ea8b301808f8
Afroz Alam <93541431+AFROZ002@users.noreply.github.com>
2023-11-02 23:05:16 +0530

    Add files via upload
    Final commit

---
+++ Natural_Number_Sum_In_Given_Range.java
@@ -0,0 +1,13 @@
+package PrepInsta_100_Program;
+
+public class Natural_Number_Sum_In_Given_Range {
+    public static void main(String[] args) {
+        int n1=12;
+        int n2=15;
+        int sum=0;
+        for (int i=n1;i<=n2;++i){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
